f728323a366202fd721ebd3141921d7103309777
iroid <iradtu2@gmail.com>
2024-06-27 18:46:26 +0300

    added back and front pages

---
+++ app/(back)/dashboard/layout.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from 'react'
+
+export default function layout({children}:{children:ReactNode}) {
+  return (
+    <div>
+        <h2>DASHBOARD lAYOUT</h2>
+        layout</div>
+  )
+}
